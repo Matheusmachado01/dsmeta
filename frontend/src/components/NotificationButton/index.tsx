a2c81063b882b119daa0ab4a2560d6f8c73ca461
matheus machado <matheus.machado0191@gmail.com>
2022-09-09 21:05:46 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets(recursos)/IMG/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
